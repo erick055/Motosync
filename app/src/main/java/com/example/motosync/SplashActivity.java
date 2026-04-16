92001fe2667ff4e2613f701ea02bb470e4aea094
Dann <erickpallorina05555@gmail.com>
2026-04-16 13:31:41 +0800

    Motosync

---
+++ app/src/main/java/com/example/motosync/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.motosync;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // 3000 milliseconds = 3 seconds delay
+        int SPLASH_DELAY = 3000;
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Navigate to MainActivity
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                // Finish this activity so the user can't press 'Back' to return to the loading screen
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
+}
